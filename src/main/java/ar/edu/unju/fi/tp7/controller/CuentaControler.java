46627a275488ae5303fc46344f13a18ab2b773d2
abel-mendez <abel.m.villagra@gmail.com>
2021-05-20 18:41:41 -0300

    guardando cambios del template cuenta

--- src/main/java/ar/edu/unju/fi/tp7/controller/CuentaControler.java
+++ src/main/java/ar/edu/unju/fi/tp7/controller/CuentaControler.java
@@ -1,10 +1,12 @@
 package ar.edu.unju.fi.tp7.controller;
 
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
+@Controller
 public class CuentaControler {
-
-	@GetMapping("/cuenta")
+	//Controller temporal para poder probar el template
+	@GetMapping("/cuenta/nueva")
 	public String getCuenta() {
 		return "nueva-cuenta";
 	}
