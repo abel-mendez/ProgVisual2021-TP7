49d1fa94a07ba6560535ed7dc1b8e045f86331d3
abel-mendez <abel.m.villagra@gmail.com>
2021-05-20 20:23:53 -0300

    primer commit de creacion del proyecto

---
+++ src/main/java/ar/edu/unju/fi/tp7/Tp7ProgVisual2021Application.java
@@ -0,0 +1,13 @@
+package ar.edu.unju.fi.tp7;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Tp7ProgVisual2021Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Tp7ProgVisual2021Application.class, args);
+	}
+
+}
