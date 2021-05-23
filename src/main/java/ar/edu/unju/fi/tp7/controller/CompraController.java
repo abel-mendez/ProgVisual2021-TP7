454fb5c8c911ceade1939f7ed2d9735e2f235cef
LEANDRO VICTORINO CRUZ <83726994+Leo-apu@users.noreply.github.com>
2021-05-22 21:06:21 -0300

    Agregando al controller el mapeo de modificar y eliminar.

--- src/main/java/ar/edu/unju/fi/tp7/controller/CompraController.java
+++ src/main/java/ar/edu/unju/fi/tp7/controller/CompraController.java
@@ -1,5 +1,7 @@
 package ar.edu.unju.fi.tp7.controller;
 
+import java.util.Optional;
+
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -7,6 +9,7 @@ import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
 
@@ -65,5 +68,20 @@ public class CompraController {
 	
 	}
 	
+	@GetMapping("/compra/borrar/{id}")
+	public ModelAndView getCompraDeletPage(@PathVariable(value = "id")Long id) {
+		ModelAndView modelView = new ModelAndView("redirect:/compra/lista");
+		compraService.deleteCompraById(id);
+		return modelView;
+	}
+
+	@GetMapping("/compra/editar/{id}")
+	public ModelAndView getCompraModPage(@PathVariable(value = "id")Long id) {
+		ModelAndView modelView=new ModelAndView("nuevo");
+		Optional<Compra> compra=compraService.getCompraById(id);
+		modelView.addObject("compra", compra);
+
+		return modelView;
+	}
 	
 }
